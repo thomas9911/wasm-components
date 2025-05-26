0c17e38a1d7e4351a3704fac8ad5007c5114e0b1
Thomas Timmer <thomas.timmer@bettyblocks.com>
2025-05-26 11:40:36 +0200

    feat: add elixir tests

---
+++ lib/wasm_components.ex
@@ -0,0 +1,18 @@
+defmodule WasmComponents do
+  @moduledoc """
+  Documentation for `WasmComponents`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WasmComponents.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
