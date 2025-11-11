765e49af65ead07aaefe1a69427a78fb505c0414
Thomas Timmer <thomastimmer11@hotmail.com>
2025-11-11 21:03:37 +0100

    feat: add run-with-context function to python

--- components/python/src/lib.rs
+++ components/python/src/lib.rs
@@ -5,17 +5,49 @@ pub mod bindings {
 }
 
 use crate::bindings::exports::thomastimmer::expression::expression::Guest;
+use crate::bindings::exports::thomastimmer::python::extra;
 
-use rustpython::vm;
+use rustpython::vm::{
+    self,
+    builtins::{PyDict, PyStr},
+    scope::Scope,
+    PyRef,
+};
 use rustpython::vm::{PyResult, VirtualMachine};
 
 pub struct PythonExecute;
 
-fn running(vm: &VirtualMachine, script: &str) -> PyResult<String> {
+fn load_context(vm: &VirtualMachine, scope: Scope, json_data: &str) -> PyResult<Scope> {
+    if let Ok(extra_data) = dump_json(vm, json_data) {
+        for (key, value) in extra_data.into_iter() {
+            let k = key.try_downcast::<PyStr>(vm)?;
+            scope.locals.set_item(k.as_str(), value, vm)?;
+        }
+
+        return Ok(scope);
+    }
+
+    Ok(scope)
+}
+
+fn dump_json(vm: &VirtualMachine, json_data: &str) -> PyResult<PyRef<PyDict>> {
+    let json = vm.import("json", 0)?;
+    let json_loads = json.get_attr("loads", vm)?;
+    let dict = json_loads.call((vm.ctx.new_str(json_data),), vm)?;
+    dict.try_downcast(vm)
+}
+
+fn running(vm: &VirtualMachine, script: &str, json_data: &str) -> PyResult<String> {
     let scope = vm.new_scope_with_builtins();
+    let scope = load_context(vm, scope, json_data)?;
+
     let source = script;
     let code_obj = vm
-        .compile(source, vm::compiler::Mode::BlockExpr, "<embedded>".to_owned())
+        .compile(
+            source,
+            vm::compiler::Mode::BlockExpr,
+            "<embedded>".to_owned(),
+        )
         .map_err(|err| vm.new_syntax_error(&err, Some(source)))?;
 
     let out = vm.run_code_obj(code_obj, scope)?;
@@ -25,16 +57,21 @@ fn running(vm: &VirtualMachine, script: &str) -> PyResult<String> {
 
 impl Guest for PythonExecute {
     fn run(script: String) -> Result<String, String> {
-         vm::Interpreter::with_init(Default::default(), |vm| {
+        use extra::Guest;
+
+        Self::run_with_context(script, String::new())
+    }
+}
+
+impl extra::Guest for PythonExecute {
+    fn run_with_context(script: String, json_data: String) -> Result<String, String> {
+        vm::Interpreter::with_init(Default::default(), |vm| {
             // put this line to add stdlib to the vm
             vm.add_native_modules(rustpython_stdlib::get_module_inits());
             vm.add_frozen(rustpython_pylib::FROZEN_STDLIB);
         })
-        // rustpython::InterpreterConfig::new()
-        // .init_stdlib()
-        // .interpreter()
         .enter(|vm| {
-            running(vm, &script).map_err(|e| {
+            running(vm, &script, &json_data).map_err(|e| {
                 let mut error = String::new();
                 match vm.write_exception(&mut error, &e) {
                     Ok(_) => error,
